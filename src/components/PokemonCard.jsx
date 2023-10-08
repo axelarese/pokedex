b0410546867ebff624cce15b5249f94f53a6327a
AXEL LARESE <axelarese@gmail.com>
2023-10-08 14:46:08 +0200

    Premier commit de ma branche PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+function PokemonCard() {
+    return (
+      <div>
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+        </figure>
+        <figcaption>
+            <p>Bulbasaur</p>
+        </figcaption>
+      </div>
+    );
+  }
+
+  export default PokemonCard;
