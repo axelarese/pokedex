7312389a831bfb0321559fb8bbbf93467d1c078a
AXEL LARESE <axelarese@gmail.com>
2023-10-09 18:53:17 +0200

    Push de props.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import PokemonCard from "./components/PokemonCard"
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
 
-<div>
-  <PokemonCard/>
-</div>
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+]
 
-export default PokemonCard;
+function App() {
+
+  return (
+    <div>
+      <PokemonCard pokemon = {pokemonList[0]}/>
+    </div>
+);
+
+}
+
+
+export default App;
